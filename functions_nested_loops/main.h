839cc4fda45b16be75082bc1e3e299ae9c8d02f5
ghadag11221 <10727@holbertonstudents.com>
2025-02-16 14:55:04 +0000

    h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,12 +1,6 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
+void print_alphabet(void);
+
+#endif /* MAIN_H */
