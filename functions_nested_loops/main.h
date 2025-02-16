1784d9ebfb677bcf7074f4d0753c0cda6a9bdd55
ghadag11221 <10727@holbertonstudents.com>
2025-02-16 15:37:19 +0000

    h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -9,5 +8,6 @@ int _abs(int n);
 int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void); /* Function prototype for times_table */
-
+int add(int, int); /* Function prototype for add */
+void print_to_98(int n);
 #endif /* MAIN_H */
