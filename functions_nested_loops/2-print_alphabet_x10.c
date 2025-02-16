0b5df458ff89b66a98869d08e35522b5c921f34f
ghadag11221 <10727@holbertonstudents.com>
2025-02-16 14:59:39 +0000

    h

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,18 @@
 #include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+* print_alphabet_x10 - Prints 10 times the alphabet in lowercase
+*
+* Return: Nothing
+*/
+void print_alphabet_x10(void)
 {
-    print_alphabet();
-    return (0);
+int i, j;
+for (i = 0; i < 10; i++)  /* Loop 10 times */
+{
+for (j = 'a'; j <= 'z'; j++)  /* Loop through the alphabet */
+{
+_putchar(j);  /* Print the letter */
+}
+_putchar('\n');  /* Print a new line after each alphabet */
+}
 }
