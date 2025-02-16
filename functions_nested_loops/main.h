9639f06b906edc32dd00759eee4c558a81683503
ghadag11221 <10727@holbertonstudents.com>
2025-02-16 15:27:40 +0000

    h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,6 +5,7 @@ int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int n); /* Function prototype for _abs */
+int _abs(int n);
+int print_last_digit(int n); /* Function prototype for print_last_digit */
 
 #endif /* MAIN_H */
