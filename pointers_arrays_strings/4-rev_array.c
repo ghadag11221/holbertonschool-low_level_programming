cc05d754289879eda1836505f7a87942688150a6
ghadag11221 <10727@holbertonstudents.com>
2025-02-25 10:49:54 +0000

    update

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -9,11 +9,12 @@
  */
 void reverse_array(int *a, int n)
 {
-int temp, i, j;
-for (i = 0, j = n - 1; i < j; i++, j--)
-{
-temp = a[i];
-a[i] = a[j];
-a[j] = temp;	
-}
+	int temp, i, j;
+
+	for (i = 0, j = n - 1; i < j; i++, j--)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
 }
