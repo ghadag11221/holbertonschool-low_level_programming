6a902e8b08ca25f48879d6f4b4a77b983b2780be
ghadag11221 <10727@holbertonstudents.com>
2025-02-25 08:58:48 +0000

    update

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,6 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-void print_triangle(int size);
-
-#endif /* MAIN_H */
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_triangle(2);
+    print_triangle(10);
+    print_triangle(1);
+    print_triangle(0);
+    return (0);
+}
