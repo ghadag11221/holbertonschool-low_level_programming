e14395f783f90271b03a5c90b01cb2814e291331
ghadag11221 <10727@holbertonstudents.com>
2025-02-25 09:02:07 +0000

    update

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,15 +1,10 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_triangle(2);
-    print_triangle(10);
-    print_triangle(1);
-    print_triangle(0);
-    return (0);
-}
+/* Function prototype for _putchar */
+int _putchar(char c);
+
+/* Function prototype for print_triangle */
+void print_triangle(int size);
+
+#endif /* MAIN_H */
