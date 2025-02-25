2009bb2e318fb32e8372951046f53f48b3412bee
ghadag11221 <10727@holbertonstudents.com>
2025-02-25 10:55:37 +0000

    update

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function prototype for _putchar */
-int _putchar(char c);
-
-/* Function prototype for _strcat */
-char *_strcat(char *dest, char *src);
+char *cap_string(char *str);
 
 #endif /* MAIN_H */
