15b0e9a0646d00981ede9079febb32d77c1b11e3
ghadag11221 <10727@holbertonstudents.com>
2025-03-03 08:44:31 +0000

    update

---
+++ recursion/_putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to be printed
+ * Return: On success 1, on error -1.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
