7286c079c27f146c5fb1a208926bcc8a8356ca42
ghadag11221 <10727@holbertonstudents.com>
2025-03-05 07:32:51 +0000

    update

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Prints the name of the program
+ * @argc: The number of arguments (unused here)
+ * @argv: An array of strings containing the program name and arguments
+ *
+ * Return: 0 (success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
