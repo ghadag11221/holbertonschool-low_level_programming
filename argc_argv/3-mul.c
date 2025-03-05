5bd62cee6851c824143918ef584c6bfc3088efaf
ghadag11221 <10727@holbertonstudents.com>
2025-03-05 08:05:57 +0000

    update

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>  
+#include <stdlib.h>  /* For atoi */
 /**
  * main - Multiplies two numbers passed as arguments
  * @argc: The number of arguments
@@ -9,14 +9,20 @@
  */
 int main(int argc, char *argv[])
 {
+int num1;
+int num2;
+int result;
+    /* Check if the program has received exactly 3 arguments */
 if (argc != 3)
 {
 printf("Error\n");
-return (1);  
+return (1);  /* Exit with status 1 if incorrect number of arguments */
 }
-int num1 = atoi(argv[1]);  
-int num2 = atoi(argv[2]); 
-int result = num1 * num2; 
+    /* Convert the arguments from strings to integers and multiply them */
+num1 = atoi(argv[1]);  /* Convert argv[1] to an integer */
+num2 = atoi(argv[2]);  /* Convert argv[2] to an integer */
+result = num1 * num2;  /* Multiply the two integers */
+    /* Print the result */
 printf("%d\n", result);
-return (0);
+return (0);  /* Exit with status 0 on success */
 }
