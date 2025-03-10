1d526214e9e70eb1bdca7265b2c4e3226ce42f26
ghadag11221 <10727@holbertonstudents.com>
2025-03-10 08:49:24 +0000

    update

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,13 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
-/* Include the necessary libraries */
+
 #include <stddef.h>
+
 /* Define the dog structure */
-struct dog {
-char *name;
-float age;
-char *owner;
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
 };
+
 /* Add the function prototype for print_dog */
 void print_dog(struct dog *d);
+
 #endif /* DOG_H */
