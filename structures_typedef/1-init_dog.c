bf4c3d3629dfc12a17baef783930463d17ac589f
ghadag11221 <10727@holbertonstudents.com>
2025-03-10 08:41:18 +0000

    update

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,16 +1,18 @@
 #include "dog.h"
+#include <stddef.h>  /* Added to fix the NULL issue */
 /**
  * init_dog - initializes a variable of type struct dog
- * @d: pointer to the struct dog to be initialized
+ * @d: pointer to struct dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d != NULL)
+if (d != NULL)  /* Fixed: d should be checked against NULL */
 {
 d->name = name;
 d->age = age;
 d->owner = owner;
-	}
+}
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,15 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stddef.h>  /* Add this for NULL usage */
 /**
- * struct dog - structure that holds information about a dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: name of the dog's owner
+ * struct dog - structure representing a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
  */
-typedef struct dog
+struct dog
 {
 char *name;
 float age;
 char *owner;
-} dog_t;
+};
+/* Function prototype */
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /* DOG_H */
