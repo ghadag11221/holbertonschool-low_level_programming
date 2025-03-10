cae30334c655cd091cb112c67881dcb320ce6eea
ghadag11221 <10727@holbertonstudents.com>
2025-03-10 08:33:23 +0000

    update

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure that holds information about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the dog's owner
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog_t;
+#endif /* DOG_H */
