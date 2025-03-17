d81e4c4304c35a3339c89d9328f4a4a1c04345e5
ghadage11221 <10727@holbertonstudents.com>
2025-03-17 07:17:16 +0000

    update

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,40 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - performs the requested operation on two integers.
- * @argc: number of arguments passed to the program.
- * @argv: array of arguments passed to the program.
+ * main - Entry point that performs a simple arithmetic operation
+ * @argc: The number of command-line arguments
+ * @argv: The array containing command-line arguments
  *
- * Return: 0 if successful, otherwise exits with a status code.
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-int num1, num2;
-int (*operation)(int, int);  // Function pointer for the operator function.
-// Check if the correct number of arguments is passed.
+int num1, num2, result;
+int (*operation)(int, int);
 if (argc != 4)
-{      
-printf("Error\n");  // Print error message and exit with status 98.
+{
+printf("Error\n");
 exit(98);
 }
-// Convert arguments from string to integer.
 num1 = atoi(argv[1]);
 num2 = atoi(argv[3]);
-// Get the correct function for the operator.
 operation = get_op_func(argv[2]);
-// If the operator is invalid, print error and exit with status 99.
-// if (operation == NULL)
+if (!operation)
 {
 printf("Error\n");
 exit(99);
 }
-// Check if division or modulo by zero is attempted.
-if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
-{
-printf("Error\n");  // Print error message and exit with status 100.
-exit(100);
-}
-// Perform the operation and print the result.
-printf("%d\n", operation(num1, num2));
-return (0);  // Return success.
+result = operation(num1, num2);
+printf("%d\n", result);
+return (0);
 }
