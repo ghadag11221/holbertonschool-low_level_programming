f07f32d01671ec4af4fc8d071d757c84092ac304
ghadag11221 <10727@holbertonstudents.com>
2025-04-02 12:47:19 +0000

    update

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -18,6 +18,12 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
-size_t print_list(const list_t *h);
+/**
+ * list_len - Returns the number of elements in a linked list_t list
+ * @h: Pointer to the head of the list
+ *
+ * Return: Number of elements in the list
+ */
+size_t list_len(const list_t *h);
 
 #endif /* LISTS_H */
