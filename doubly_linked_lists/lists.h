b122a519eddf875beaa3c6c70b07ad3ac90788a2
ghadag11221 <10727@holbertonstudents.com>
2025-04-06 13:37:43 +0000

    update

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -9,7 +9,7 @@
  * @prev: pointer to the previous node
  * @next: pointer to the next node
  *
- * Description: node structure for a doubly linked list
+ * Description: node structure for a doubly linked list project
  */
 typedef struct dlistint_s
 {
@@ -22,5 +22,6 @@ typedef struct dlistint_s
 size_t print_dlistint(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
 
 #endif /* LISTS_H */
